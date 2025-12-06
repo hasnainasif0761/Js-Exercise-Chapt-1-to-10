3bf041e38053de323ba4ab3e12f3d4f0bd1ccc5d
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-06 21:16:46 +0500

    'Js-Exercise-Chapt-1-to-10'

---
+++ script.js
@@ -0,0 +1,113 @@
+// Javascript Exercise Question Chapter 1 to 10
+
+// Question no 1) Alerts
+// Anwere for this question is few points
+// 1)
+//  alert('Hello World!');   
+// 2)
+//  alert('Huzaifa Asif');   
+// 3)
+//  alert(2);                
+// 4)
+//   var name = 'Huzaifa';   
+// console.log('Hello '+name);
+// 5)
+//  alert('Hello '+'World');  
+
+//Question no 2) Variable for String:
+// Anwere for this question is few points
+// 1)
+//  var userName = 'Huzaifa Asif';    
+// 2)
+//  var message = 'Welcome to Javascript Chapters';   
+// alert(message);
+// 3)
+//  var firstName = "huzaifa";    
+// var lastName = 'Asif';
+// 4)
+//  var quote = 'Hard work pays off.';    
+// 5)
+//  var city = 'Karachi';     
+// alert('Welcome to '+city);
+
+
+// Question no 3) Variable for Number
+// Answere for this question is few point
+// 1)
+//  var age = 18;  
+// 2)
+//  var a = 5;   
+// var b = 4;
+// var sum = a + b;
+// console.log(sum);
+// 3)
+//  var a = 3;
+// var b = 2;       // Subtract one number from another.
+// var subtract = a - b;
+// console.log(subtract);
+// 4)
+//  var a = 2;
+// var b = 3;     // Multiply two numbers stored in variables.
+// var multiply = a * b;
+// console.log(multiply);
+// 5)
+//  let age = 18;    // Show a message like: "You are 20 years old"
+// alert("You are " + age + " years old.");
+
+
+
+// Question 4) Math Expressions
+// Answere for this question is few point
+// 1)
+// var x = 10;
+// var y = 5;    // Create two variables: x = 10, y = 5 4 add them and show result.
+// var result = x + y;
+// console.log(result);
+// 2)
+// var y = 5;
+// var x = 10;  
+// var result = y - x;
+// alert(result);
+// 3)
+// var x = 10;
+// var y = 5;
+// console.log(x*y);
+// 4)
+// var x = 5;
+// var b = 2;
+// console.log(x/b);
+// 5)
+// var x = 3;
+// var y = 5;
+// var result = x % y;
+// console.log(result);
+
+
+// Question 5) Prompt
+// Answere for this question is few point
+// 1)
+// var name = prompt('What is your Name');       
+// alert("Hello "+name);
+// 2)
+// var age = Number(prompt('Enter your Age')); 
+// console.log(age);
+// 3)
+// var num = +prompt('Enter your Number');
+// var result = num * 2
+// console.log("Your Output is "+result);
+// 4)
+// var color = prompt('Enter your Favourite Color ?');
+// alert('WOW! I Like '+color+' too');
+// 5)
+// var city = prompt('Enter your city name');
+// console.log('Your are from '+city);
+
+
+// Question 6) If Statement and Comparison Operators:
+
+
+
+
+
+
+
